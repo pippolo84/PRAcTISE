4aa3c449bcad4a396737a80660e2925fb40721d7
Fabio Falzoi <falzoi@feanor.sssup.it>
2012-07-03 10:47:41 +0200

    Minor fixes to license text

--- include/flat_combining.h
+++ include/flat_combining.h
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
+ * along with PRAcTISE.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __FLAT_COMBINING_H
